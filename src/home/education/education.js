257a61b0e6f17a55cd0735ead7b86498fdbce3d5
Michael Mitrakos <mike.mitrakos@verys.com>
2017-09-23 15:27:29 -0700

    [update]

--- src/home/education/education.js
+++ src/home/education/education.js
@@ -14,25 +14,20 @@ class Education extends Component {
   }
 
   render() {
-
     return (
       <div className="educationContainer">
-      
         <div className="educationContainer-timeline">
           <div className="educationContainer-timeline-leftCircle">
             <div className="educationContainer-timeline-leftCircle-filling" />
           </div>
-          
           <div className="educationContainer-timeline-middleCircle">
             <div className="educationContainer-timeline-middleCircle-filling" />
-          </div>
-          
+          </div>   
           <div className="educationContainer-timeline-rightCircle">
             <div className="educationContainer-timeline-rightCircle-filling" />
           </div>
           <div className="educationContainer-timeline-meterFill" />
         </div>
-
       </div>
     );
   }
